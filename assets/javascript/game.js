a5c8b6345056668ddd15c3a6a0775c509a06d46b
Tatiana Marsh <tatiana.alcocer@gmail.com>
2016-08-27 19:31:05 -0400

    Game logic with win/loss tracking

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,26 +2,50 @@ $(document).ready(function(){
 
 
 // Generate a random number, store it in a variable
-var cell1 = Math.floor(Math.random() * 12) + 1; 
-var cell2 = Math.floor(Math.random() * 12) + 1;
-var cell3 = Math.floor(Math.random() * 12) + 1;
-var cell4 = Math.floor(Math.random() * 12) + 1;
-
 var score = 0;
-var wins = 0;
-var losses = 0;
+var winCount = 0;
+var lossCount = 0;
+var scoreGoalMax = 120;
+var scoreGoalMin = 19;
+
+var cell1 = Math.ceil(Math.random() * 12); 
+var cell2 = Math.ceil(Math.random() * 12);
+var cell3 = Math.ceil(Math.random() * 12);
+var cell4 = Math.ceil(Math.random() * 12);
+
+var winScore = Math.ceil(Math.random() * scoreGoalMax - scoreGoalMin) + scoreGoalMin;
+
 
 
 // FUNCTIONS: 
-function updateScore(score, number){
-	return score + number;
+function updateScore(score, amount){
+	return score + amount;
 }
 
-// On click of crystals cell 1: 
+
+function checkWin(winScore, score) {
+	
+	if (score == winScore) {
+		return "win";
+
+	} else if (score > winScore) {
+		return "loss";
+
+	} else if (score < winScore) {
+		return "continue";
+	}
+}
+
+function renderRunningScore(winCount, lossCount) {
+	$("#running-score").html("WINS: " + winCount + "<br/>" + "LOSSES: " + lossCount);
+}
+
+
+// ON CLICK EVENT OF CRYSTALS CELLS: 
  $(".crystals-cells").on("click", function(event) {
 
-
  	var id = event.currentTarget.id;
+ 	
  	var amount = 0;
 
  	switch(id) {
@@ -47,15 +71,49 @@ function updateScore(score, number){
  			break;
  	}
 
- 	// Add the value of cell1 to var score
+ 	// UPDATE SCORE WITH VALUE OF CELL CLICKED (AMOUNT)
  	score = updateScore(score, amount);
 
-    // Then dump the random number into our active-score div. 
-    $('#active-score').html("Active Score: " + score);
+    // UPDATE #ACTIVE SCORE DIV WITH UPDATED SCORE 
+    $('#active-score').html("ACTIVE SCORE: " + score);
 
-    console.log("cell1: " + cell1);
+
+
+   	// CHECK FOR WIN
+   	// If you won... 
+   	if (checkWin(winScore, score) == "win") {
+   		console.log("you won");
+
+   		// Increase winCount by 1
+   		winCount++;
+		renderRunningScore(winCount, lossCount);
+
+   		// Reset score to 0, print new active score
+   		score = 0;
+   		$('#active-score').html("ACTIVE SCORE: " + score);
+
+   	// If you lose...
+   	} else if (checkWin(winScore, score) == "loss") {
+   		console.log("you lost");
+
+   		// Increase lossCount by 1
+   		lossCount++; 
+		renderRunningScore(winCount, lossCount);
+
+   		// Reset the score to 0, print new active score
+   		score = 0;
+   		$('#active-score').html("ACTIVE SCORE: " + score);
+
+   	// If 'continue', nothing happens 
+   	} else if (checkWin(winScore, score) == "continue") {
+   		console.log("continue");
+   	}
+
+
     console.log("score: " + score);
-})
+    console.log("winScore: " + winScore);
+
+}) // END OF CLICK EVENT FOR CRYSTALS CELLS
 
 
 
